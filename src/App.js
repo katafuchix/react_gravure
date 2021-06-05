1ba6fda2a85ec0132240d21107d800fd573ccc62
katafuchix <katafuchix@gmail.com>
2021-06-05 22:51:41 +0900

    SplitPane sample

--- src/App.js
+++ src/App.js
@@ -1,16 +1,80 @@
 import Sidebar from './modules/sidebar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router,
+          Switch,
+          Route,
+          Link
+        } from 'react-router-dom';
 
+import Home from './Home'
+import Sample from './sample';
+
+import SplitPane from 'react-split-pane';
+
+/*
 function App() {
   return (
     <Router>
       <div className="App">
         <Sidebar />
+        <Switch>
+           <Route path="/profile">
+             <Home />
+           </Route>
+           <Route path="/login">
+             <Home />
+           </Route>
+         </Switch>
       </div>
     </Router>
   );
 }
+*/
+const styles = {
+  background: '#000',
+  width: '2px',
+  cursor: 'col-resize',
+  margin: '0 5px',
+  height: '100%',
+};
 
+
+function App() {
+  return (
+  <Router>
+    <SplitPane
+      split="vertical"
+      minSize={100}
+      defaultSize={100}
+      resizerStyle={styles}
+    >
+      <menu>
+        <div><Link to="/">Home</Link></div>
+        <div><Link to="/topics">Topics</Link></div>
+      </menu>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/topics" component={Home} />
+      </div>
+    </SplitPane>
+  </Router>
+);
+}
+/*
+function App() {
+  const style = {
+    width: "50%",
+    margin: "0 auto",
+    marginTop: 150
+  };
+  return (
+    <div className="App">
+      <div style={style}>
+        <Sample />
+      </div>
+    </div>
+  );
+}
+*/
 export default App;
 
 /*
---
+++ src/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Home() {
+  return <div className="Page">Home</div>;
+}
+export default Home;
